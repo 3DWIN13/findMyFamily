1890a790db33ceae162a2b3a093f5fc1556ba08b
LisbethC11 <60081288+LisbethC11@users.noreply.github.com>
2020-03-12 06:51:10 +0100

    f

--- librerias/config_x.php
+++ librerias/config_x.php
@@ -1,8 +1,14 @@
-    <?php
+     <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','proyecto');
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'proyecto';
 
-define('IS_DEBUG', true);
+try{
+    $conn = new PDO("mysql:host=$server;dbname=$database;",$username, $password);
+}catch(PDOException $e){
+    die('Conecction Failed: '.$e->getMessage());
+}
+
+?>
--- login.php
+++ login.php
@@ -1,17 +1,45 @@
 <?php
 ////////////INICIO DEL HTML CON EL NAVBAR//////////////////
 require('librerias/motor.php');
+session_start();
 
+
+
+if (!empty($_POST['email']) && !empty($_POST['password'])){
+  $records = $conn->prepare('SELECT id, email, password FROM usuario WHERE email=:email');
+  $records->bindParam(':email', $_POST['email']);
+  $records->execute();
+  $result = $records->fetch(PDO::FETCH_ASSOC);
+
+  if(count($result) > 0 && password_verify($_POST['password'], $result['password'])){
+  $_SESSION['usuario_id'] = $result['id'];
+  header('Locationn: /php-Agregar'); 
+  header('Location: http://localhost/findMyFamily/register.php'); 
+  }else{
+ $message='no';
+  }
+}
 echo start(); 
 
  echo nav();
 ?>
 
-
-
-<section class="hero-wrap js-fullheight img" style="background-image: url(images/bg_3.jpg);">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php if (!empty($messsage)) : ?>
+    <p><?=   $message  ?></p>
+  <?php endif; ?>
+<form action="Agregar.php" method="post">
+<section  class="hero-wrap js-fullheight img" style="background-image: url(images/loginn.png);">
 <div class="overlay"></div>
     <div class="container">
+    
     <br>
 		  <br>
 		  <br>
@@ -20,24 +48,14 @@ echo start();
 		  <br>
           <br>
           
-      <div class="row">
+      <div class="row" style="width: 1350px;">
         <div class="col-md-4 mb-4 mb-md-0">
           <div class="card card-login py-4">
+          
             <form class="form-login" method="" action="">
               <div class="card-header card-header-primary text-center">
                 <h4 class="card-title">Login</h4>
-                <div class="social-line">
-                  <a href="https://www.facebook.com/criismlin" class="btn-icon d-flex align-items-center justify-content-center">
-                    <i class="ion-logo-facebook"></i>
-                  </a>
-                  <a href="https://www.facebook.com/criismlin" class="btn-icon d-flex align-items-center justify-content-center">
-                    <i class="ion-logo-twitter"></i>
-                  </a>
-                  <a href="https://www.facebook.com/criismlin" class="btn-icon d-flex align-items-center justify-content-center">
-                    <i class="ion-logo-googleplus"></i>
-                  </a>
-                </div>
-              </div>
+
               <div class="card-body p-4">
                 <div class="input-group mb-3">
                   <div class="input-group-prepend">
@@ -53,7 +71,7 @@ echo start();
                       <i class="ion-ios-paper-plane"></i>
                     </span>
                   </div>
-                  <input type="email" class="form-control" placeholder="Email...">
+                  <input type="email"  name="email" class="form-control" placeholder="Email...">
                 </div>
                 <div class="input-group mb-3">
                   <div class="input-group-prepend">
@@ -61,9 +79,22 @@ echo start();
                       <i class="ion-ios-lock"></i>
                     </span>
                   </div>
-                  <input type="password" class="form-control" placeholder="Contraseña...">
+                  <input type="password" name="password" class="form-control" placeholder="Contraseña...">
                 </div>
+                <div class="container">
+                <div class="row justify-content-md-center">
+               
+                  <input type="submit" class="btn btn-light " value="Iniciar Sesion">
+               
               </div>
-</div>
+              </div>
+              </div>
+              
+              </div>
+</form>
+
 </section>
+</body>
+</html>
+
 <?= /* fin del html */ finaly(); ?>
