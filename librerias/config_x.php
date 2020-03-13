573892167c8c71553a149c7518cd2f3fe568a052
3DWIN13 <ecolladocastillo@gmail.com>
2020-03-13 13:45:49 -0400

    Update config_x.php

--- librerias/config_x.php
+++ librerias/config_x.php
@@ -1,9 +1,11 @@
      <?php
 
-$server = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'proyecto';
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','inta');
+
+define('IS_DEBUG', true);
 
 try{
     $conn = new PDO("mysql:host=$server;dbname=$database;",$username, $password);
